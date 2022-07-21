c315419fe97fcf79706d41649116efa67c2e9002
htmlHxcker <clintonadeleke@gmail.com>
2022-07-21 12:17:36 +0100

    [FEAT]:Import projects stylesheet

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 import '../styles/index.css';
 import '../styles/resets.css';
 import '../styles/utilities.css';
+import '../styles/projects.css';
 
 console.log(`
  #     #                                                                                                                                 
